d509e6a810707a182b363f2626381ac6f60e86a9
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-17 13:16:52 -0500

    player attack and assess damage enemy attack and assess damage

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,39 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-//this creates a function named "fight"
+var playerHealth = 100;
+var playerAttack =10;
 
-function fight(){
-    window.alert("The fight has begun!");
-}
-//fight();
+//you can also log multiple values at once
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName="Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+     //check enemy's health
+     if(enemyHealth<=0) {
+        window.alert(enemyName + " has died!");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+   // check player's health
+   if(playerHealth<=0){
+       window.alert(playerName + " has died");
+   }
+   else{
+       window.alert(playerName + " still has " + playerHealth + " health left.");
+   }
+
+};
+fight();
+
