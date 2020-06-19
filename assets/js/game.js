896f2250883bba8d820cf1e2cd3f7682a4e6e9af
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-19 09:47:44 -0500

    MVP 1

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ var playerAttack =10;
 var playerMoney = 10;
 
 var enemyNames=[ "Roborto", "Amy Andriod", "Robo Trumble"];
-var enemyHealth = 12;
+var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
@@ -25,12 +25,13 @@ var fight = function(enemyName) {
            }
         }
             //player attacks enemy
-            enemyHealth = enemyHealth - playerAttack;
+         else if(promptFight === "fight" || "FIGHT") {
+                enemyHealth = enemyHealth - playerAttack;
                 console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
                 );
-            //check enemy's health
-            if(enemyHealth<=0) {
+                //check enemy's health
+                if(enemyHealth<=0) {
                  window.alert(enemyName + " has died!");
                  
                  // awared player money for winning
@@ -38,48 +39,76 @@ var fight = function(enemyName) {
 
                  //leave while loop, enemy is dead
                 break;
-            }
-            else{
+                }
+                else{
                 window.alert(enemyName + " still has " + enemyHealth + " health left.");
-            }
-            //enemy attacks player
-            playerHealth = playerHealth - enemyAttack;
-            console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-            );
-            // check player's health
-            if(playerHealth<=0){
+                }
+        
+                //enemy attacks player
+                playerHealth = playerHealth - enemyAttack;
+                console.log(
+                enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+                );
+                // check player's health
+                if(playerHealth<=0){
                 window.alert(playerName + " has died");
                 break;
-            }
-            else{
+                }
+                else{
                  window.alert(playerName + " still has " + playerHealth + " health left.");
-            } 
+                } 
+         }
+         else {
+             window.alert("You need to pick a valid option. Try Again!");
+         }
         }
     }
+       var startGame = function() {
+           //reset player stats
+           playerHealth = 100;
+           playerAttack = 10;
+           playerMoney = 10;
         for(var i = 0; i<enemyNames.length; i++){
             //let user know what round they are in, remember that arrays start at 0
             if(playerHealth > 0){
                 window.alert("Welcome to Robot Gladiators! Round " + ( i+1));
-            }
+
+                var pickedEnemyNames = enemyNames[i];
+                enemyHealth = 20
+                //use debugger to pause script from running and check whats wrong
+                //debugger    
+
+                //pass the pickedEnemyNames variable's value into the fight function 
+                fight(pickedEnemyNames);
+            }   
             else{
                 window.alert("You had lost your robot in battle! Game Over!");
                 break;
+                }
             }
-            //pick new enemy to fight based on the index of the enemyNames array
-            var pickedEnemyNames = enemyNames[i];
-            
-            //reset enemyHealth before starting new fight
-            enemyHealth = 50;
+        endGame();
+       };
+       
+       var endGame = function() {
+           if (playerHealth>0){
+                window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+           }
+           else{
+                window.alert("You've lost your robot in battle.");
+           }
+           var playAgainConfirm = window.confirm("Would you like to play again?");
 
-            //use debugger to pause script from running and check whats wrong
-            //debugger    
+            if(playAgainConfirm){
+                 startGame();
+            }
+             else{
+                window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+            }
+       }
 
-            //pass the pickedEnemyNames variable's value into the fight function 
-            fight(pickedEnemyNames);
-        }
+       
 
-//fight();
+startGame();
 
 // Game States
 // "WIN" - Player robot has defeated all enemy robots
